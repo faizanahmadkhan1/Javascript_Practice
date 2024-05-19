2e07b4823ac6bd4ab0b9f21a55bd07e14099e5ef
Faizan Ahmad Khan <faizan.ahmad.khan@outlook.com>
2024-05-19 12:27:59 +0530

    data types with code

--- Getting started with js/Introduction to Javascript/DataTypes.js
+++ Getting started with js/Introduction to Javascript/DataTypes.js
@@ -24,10 +24,28 @@ var largeNum= 22228778522123321458958n
 console.log(largeNum, typeof largeNum);
 
 //* 5. Symbol
+var unique = Symbol("my-key");
+console.log(unique, typeof unique);
+var another  =Symbol("my-key");
+console.log(another, typeof another);
+console.log(unique===another);
 
 
 //% 6. Null
+var address= null;
+console.log(address, typeof address);
+
 //% 7. Undefined
+var undef = undefined;
+console.log(undef, typeof undef);
+
+
 //# 8. Objects
+var Employee1= {Name: "Amir", Id:23445, Age:null };
+console.log(Employee1, typeof Employee1);
+var Employee2 = {Name: "Nehal", Id:23446, Age:undefined };
+console.log(Employee2, typeof Employee2);
+console.log(Employee1.Age);
+console.log(Employee2.Age);
 
 
