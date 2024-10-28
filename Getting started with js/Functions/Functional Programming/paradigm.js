32347a14206a9486c44d27e7122a3a0fb7f78069
Faizan Ahmad Khan <59918587+fakhan1995@users.noreply.github.com>
2024-10-28 10:56:52 +0530

    Completed functional programming and rearranged folder

--- Getting started with js/Functions/Functional Programming/paradigm.js
+++ Getting started with js/Functions/Functional Programming/paradigm.js
@@ -13,10 +13,42 @@ function accumulate (num) {
     total += num;
     return total;
 }
-console.log(accumulate(5));
-console.log(accumulate(6));
-console.log(accumulate(10));
+//console.log(accumulate(5));
+//console.log(accumulate(6));
+//console.log(accumulate(10));
 
+//~* Pure Function: does not use / Modify outside data 
+function accumulate (total,value) {
+    return total + value;
 
+}
+var total=0;
+console.log(accumulate(total,5));
+console.log(accumulate(total,7));
+console.log(accumulate(total,1));
 
+//~*2 Higher Order Fumction (HOF)- Pass function to another function
+const expenses= [
+    {txn:1, desc:"Zomato",amount:350},
+    {txn:2, desc:"Amazon",amount:1150},
+    {txn:3, desc:"Uber",amount:550}
 
+];
+//* reduce is a HOF 
+const sum= expenses.reduce((total,exp)=>accumulate(total,exp.amount),0);
+
+console.log("SUM",sum);
+
+//!3. Lambda (Fat arrow Function)
+
+function sqr(x) {
+    return x*x;
+}
+console.log("Square of (5)=", sqr(5));
+// Rewriting as Lambda function
+function sqr(x) {return x*x;} // Function Declaration
+var sqr=function(x) {return x*x;} // Function Expression
+var sqr=(x)=> {return x*x;} //Lambda Expression 
+// IF we have single statement and single () we can drop it 
+// concise lambda function
+var sqr= x =>  x * x;
